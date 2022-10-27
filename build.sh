d1c6712a363f1daaef4036369deb8e829fe6d5b3
Sever7n <bastianwolter@gmx.net>
2022-10-28 00:14:07 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 cargo build --release
 sudo cp target/release/linux_creation_tool /usr/bin/
 
-mkdir /etc/linux_creation_tool/pictures
-cp pictures/*.png /etc/linux_creation_tool/pictures/
-cp config.json /etc/linux_creation_tool/
+sudo mkdir /etc/linux_creation_tool/pictures
+sudo cp pictures/*.png /etc/linux_creation_tool/pictures/
+sudo cp config.json /etc/linux_creation_tool/
 
-cp pictures/icon.png /usr/share/pixmaps/sev.linux_creation_tool.png
-cp sev.linux_creation_tool.desktop /usr/share/applications/
+sudo cp pictures/icon.png /usr/share/pixmaps/sev.linux_creation_tool.png
+sudo cp sev.linux_creation_tool.desktop /usr/share/applications/
