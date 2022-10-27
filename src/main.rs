de0121974317efb7272a8cc5be3acb8d258c31ac
Sever7n <bastianwolter@gmx.net>
2022-10-28 00:06:59 +0200

    Added Images

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let img = img.as_rgba8().unwrap().as_raw();
 
     let mut settings = Settings::default();
-    settings.flags = Flags::new(client, format!("{}{}", DIRECTORY, "config.json").into());
+    settings.flags = Flags::new(client, "/etc/linux_creation_tool/");
     settings.window.size = (512, 362);
     settings.exit_on_close_request = true;
     settings.window.icon = Some(Icon::from_rgba(img.clone(), 256, 256).unwrap());
