8548e1ff79317bc6f75aedd72b6fc1552fd49875
Severin <bastianwolter@gmx.net>
2022-10-27 23:57:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cargo build --release
 cp target/release/linux_creation_tool /usr/bin/
 
 mkdir /etc/linux_creation_tool/pictures
-cp pictures/*.png /etc/linux_creation_tool/pictures
+cp pictures/*.png /etc/linux_creation_tool/pictures/
 cp config.json /etc/linux_creation_tool/
 
 cp pictures/icon.png /usr/share/pixmaps/sev.linux_creation_tool.png
